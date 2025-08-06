cee60ca892989bebbeb224260bdbe1ab4acab2d7
LucasSim0n <lucassimonfeijoo@gmail.com>
2025-08-06 15:33:12 +0200

    exit and help

---
+++ commands.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!\nUsage:\n")
+	for _, c := range Commands {
+		fmt.Printf("%s: %s\n", c.name, c.description)
+	}
+	return nil
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var Commands map[string]cliCommand
+
+func init() {
+	Commands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("hello world")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex >")
+		scanner.Scan()
+		stripText := strings.TrimSpace(scanner.Text())
+		input := strings.Split(strings.ToLower(stripText), " ")[0]
+
+		c, ok := Commands[input]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+		err := c.callback()
+		if err != nil {
+			fmt.Printf("Error occured with command %s: %v", c.name, err)
+		}
+	}
 }
